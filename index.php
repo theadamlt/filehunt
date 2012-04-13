a5c3a699d3b0714737ba4a23061992983260da97
Adam Lilienfeldt <lilienfeldt.adam@gmail.com>
2012-04-13 08:27:33 +0200

    commit

--- index.php
+++ index.php
@@ -7,16 +7,6 @@
 	if($_SERVER['HTTP_HOST'] != 'localhost')
 	{
 		echo "
-<script type='text/javascript'>
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-29716641-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
 <script src=\"js/main.min.js\"></script><script src=\"js/lib.min.js\"></script><link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.min.css\" />";
 }
 else echo '<script src="js/main.js"></script><script type="text/javascript" src="js/lib.js"></script><link rel="stylesheet" type="text/css" href="css/style.css" />';?>
@@ -45,8 +35,21 @@ navBar();
 header('Content-Type: text/html; charset=utf-8');
 header('Cache-control: public');
 header('Vary: Accept-Encoding');
+
 require_once('main.php');
 ?>
+<script type='text/javascript'>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-29716641-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
+
 <br />
 <br />
 <? if($_SERVER['HTTP_HOST'] != 'localhost') echo '
